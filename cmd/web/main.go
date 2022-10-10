18ebb42c98983a08e3347ff09b74dad6d33bd65f
mcsymiv <s.mcsymiv@gmail.com>
2022-10-10 14:39:22 +0300

    add app config, add repository for handlers

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,18 +2,36 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 
+	"github.com/mcsymiv/web-hello-world/pkg/config"
 	"github.com/mcsymiv/web-hello-world/pkg/hand"
+	"github.com/mcsymiv/web-hello-world/pkg/render"
 )
 
 const port = ":8080"
 
 func main() {
-	http.HandleFunc("/", hand.Index)
-	http.HandleFunc("/home", hand.Home)
-	http.HandleFunc("/about", hand.About)
-	http.HandleFunc("/exit", hand.Exit)
+	var app config.AppConfig
+
+	tmplCache, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal("Can not create template from cache")
+	}
+
+	app.UseCache = false
+	app.TemplateCache = tmplCache
+
+	repo := hand.NewRepo(&app)
+	hand.NewHandlers(repo)
+
+	render.NewTemplates(&app)
+
+	http.HandleFunc("/", hand.Repo.Index)
+	http.HandleFunc("/home", hand.Repo.Home)
+	http.HandleFunc("/about", hand.Repo.About)
+	http.HandleFunc("/exit", hand.Repo.Exit)
 
 	fmt.Println("Started app.\nListen on port :8080")
 	_ = http.ListenAndServe(port, nil)
