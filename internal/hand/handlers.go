04f30c97df5d8219a1583f4192aea88a069211ab
mcsymiv <s.mcsymiv@gmail.com>
2023-10-28 19:45:47 +0000

    add redirect /home

--- internal/hand/handlers.go
+++ internal/hand/handlers.go
@@ -49,8 +49,7 @@ func NewHandlers(r *Repository) {
 
 // Index renders home page and puts user id into session
 func (repo *Repository) Index(w http.ResponseWriter, r *http.Request) {
-	//http.Redirect(w, r, "/home", http.StatusSeeOther)
-	render.Template(w, r, "index.page.tmpl", &models.TemplateData{})
+	http.Redirect(w, r, "/home", http.StatusSeeOther)
 }
 
 // Home renders home MyQ page
